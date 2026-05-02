263920dd54a1d2e57e94c90470e0dd82a1f2e751
Anji_yepuganti <anjicherry28@gmail.com>
2026-05-02 21:43:28 +0530

    Create half_adder_tb.v

---
+++ half_adder_tb.v
@@ -0,0 +1,16 @@
+module half_adder_tb;
+    reg A, B;
+    wire Sum, Carry;
+    half_adder uut(
+        .A(A),
+        .B(B),
+        .Sum(Sum),
+        .Carry(Carry));
+    initial begin
+        A = 0; B = 0; #10;
+        A = 0; B = 1; #10;
+        A = 1; B = 0; #10;
+        A = 1; B = 1; #10;
+        $stop;
+    end
+endmodule
