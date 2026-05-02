27e0c51115940579963734eeef1fee33c7d56e16
Anji_yepuganti <anjicherry28@gmail.com>
2026-05-02 21:33:40 +0530

    Create nand_gate_tb.v

---
+++ nand_gate_tb.v
@@ -0,0 +1,16 @@
+module nand_gate_tb;
+    reg a;
+    reg b;
+    wire y;
+    nand_gate uut(
+        .a(a),
+        .b(b),
+        .y(y));
+    initial begin
+        a = 0; b = 0; #10;
+        a = 0; b = 1; #10;
+        a = 1; b = 0; #10;
+        a = 1; b = 1; #10;
+        $stop;
+    end
+endmodule
